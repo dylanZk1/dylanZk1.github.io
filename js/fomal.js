955a31b7337ba3c3dc0e162346ad591ea2f57ae5
dylanZk1 <q8w230@163.com>
2023-07-17 19:10:13 +0800

    备案信息更新

--- js/fomal.js
+++ js/fomal.js
@@ -104,6 +104,10 @@ function changeBeiAn() {
     BeiAnImg.setAttribute("src","/assets/BeiAn.svg")
     BeiAnImg.setAttribute("data-lazy-src","/assets/BeiAn.svg")
     BeiAnUri.setAttribute("href","https://icp.gov.moe/?keyword=20230291")
+  }else if(window.location.host.toString().indexOf("drobot.online")!=-1){
+    BeiAnImg.setAttribute("src","/assets/SuBeiAn.svg")
+    BeiAnImg.setAttribute("data-lazy-src","/assets/SuBeiAn.svg")
+    BeiAnUri.setAttribute("href","javascript:void(0)")
   }
 }
 
@@ -314,7 +318,7 @@ document.addEventListener('pjax:complete', getWeibo);
 document.addEventListener('DOMContentLoaded', getWeibo);
 
 function getWeibo() {
-  fetch('https://weibo-top-api-gules.vercel.app/api').then(data => data.json()).then(data => {  // 这里要写上你的API!!!
+  fetch('https://weibo.'+ window.location.host.toString() + '/api').then(data => data.json()).then(data => {  // 这里要写上你的API!!!
     let html = '<style>.weibo-new{background:#ff3852}.weibo-hot{background:#ff9406}.weibo-jyzy{background:#ffc000}.weibo-recommend{background:#00b7ee}.weibo-adrecommend{background:#febd22}.weibo-friend{background:#8fc21e}.weibo-boom{background:#bd0000}.weibo-topic{background:#ff6f49}.weibo-topic-ad{background:#4dadff}.weibo-boil{background:#f86400}#weibo-container{overflow-y:auto;-ms-overflow-style:none;scrollbar-width:none}#weibo-container::-webkit-scrollbar{display:none}.weibo-list-item{display:flex;flex-direction:row;justify-content:space-between;flex-wrap:nowrap}.weibo-title{white-space:nowrap;overflow:hidden;text-overflow:ellipsis;margin-right:auto}.weibo-num{float:right}.weibo-hotness{display:inline-block;padding:0 6px;transform:scale(.8) translateX(-3px);color:#fff;border-radius:8px}</style>'
     html += '<div class="weibo-list">'
     let hotness = {
